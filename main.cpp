0a178349b2bc70a42646c7a0c0fb72f2c69d2c6d
K4m3ledd1in3 <s309439@studenti.polito.it>
2025-03-29 16:47:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,14 +24,14 @@ int main(void)
 	MatrixXd X(2,1);  	
 	X << -1,-1;   
 	MatrixXd A(2,2);
-    std::cout << std::setprecision(_prec) << std::scientific; 
+       std::cout << std::setprecision(_prec) << std::scientific; 
 	
 	cout << endl << "(1)" << endl;
 	A << 5.547001962252291e-01, -3.770900990025203e-02, 8.320502943378437e-01, -9.992887623566787e-01 ;
 	MatrixXd b(2,1);
 	b << -5.169911863249772e-01, 1.672384680188350e-01;
 	PartialPivLU<MatrixXd> LU(A);
-    Factor_LU(LU,L,U,A);
+        Factor_LU(LU,L,U,A);
 	Solve_System_LU(L,U,LU,b,x);
 	cout << "xLU: " <<endl<< x << endl;
 	Error(X,x,"PA=LU");
@@ -46,7 +46,7 @@ int main(void)
 	b << -6.394645785530173e-04, 4.259549612877223e-04;
 	
 	PartialPivLU<MatrixXd> LU1(A);
-    Factor_LU(LU1,L,U,A);
+        Factor_LU(LU1,L,U,A);
 	Solve_System_LU(L,U,LU1,b,x);
 	
 	cout << "xLU: " <<endl<< x << endl;
@@ -61,7 +61,7 @@ int main(void)
 	b << -6.400391328043042e-10, 4.266924591433963e-10;
 	
 	PartialPivLU<MatrixXd> LU2(A);
-    Factor_LU(LU2,L,U,A);
+        Factor_LU(LU2,L,U,A);
 	Solve_System_LU(L,U,LU2,b,x);
 	
 	cout << "xLU: " <<endl<< x << endl;
@@ -77,12 +77,12 @@ int main(void)
 
 void Solve_System_LU(const MatrixXd& L, const MatrixXd& U,const PartialPivLU<MatrixXd>& LU, const MatrixXd& b, MatrixXd& out){
 	MatrixXd y = L.inverse()*LU.permutationP()*b;
-    out = U.inverse()*y;
+         out = U.inverse()*y;
 }
 
 void Factor_LU(const PartialPivLU<MatrixXd>& LU, MatrixXd& L, MatrixXd& U,const MatrixXd& A){
     U = LU.matrixLU().triangularView<Upper>();     
-	L = MatrixXd::Identity(A.rows(), A.cols());
+    L = MatrixXd::Identity(A.rows(), A.cols());
     L.triangularView<StrictlyLower>() = LU.matrixLU();
 }
 
